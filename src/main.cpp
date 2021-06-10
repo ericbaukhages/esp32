1b7b3098472f87b9747bde7b76317e9c44d414da
Eric Baukhages <eric.baukhages@gmail.com>
2021-06-09 23:20:26 -0400

    Add a cycle button and counter to display on segment

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,33 @@
 #include "Arduino.h"
 #include "segment.h"
 
+#define CYCLE_BUTTON 34
+
+int counter;
+
 void setup()
 {
+  pinMode(CYCLE_BUTTON, INPUT_PULLUP);
   initializeSegments();
+
+  counter = 0;
 }
 
 void loop()
 {
-  for (int i = 0; i < 16; i++) {
-    writeNumberToSegment(i);
-    delay(1000);
-    clearSegment();
+  int cycleButtonState = digitalRead(CYCLE_BUTTON);
+
+  if (cycleButtonState == HIGH)
+  {
+    counter++;
+    delay(150);
   }
+
+  if (counter >= 16)
+  {
+    counter = 0;
+  }
+
+  clearSegment();
+  writeNumberToSegment(counter);
 }
